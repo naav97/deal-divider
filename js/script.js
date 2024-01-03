27a80138b0f6f9464a23bc15a916779132e0fc15
Nicolas Alvarado <nicolas@grows.pro>
2024-01-03 09:45:35 -0500

    arreglo suma

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const total = document.getElementById('imp-amo').value;
     for (let i = 1; i <= cuotas; i++) {
         let inp = document.getElementById('imp-cuo-'+i);
-        inp.addEventListener('change', checkTot(cuotas, total));
+        inp.addEventListener('change', () => {checkTot(cuotas, total)});
     }
 });
 
 const checkTot = (cuotas, total) => {
     let presTot = 0;
     for (let i = 1; i <= cuotas; i++) {
-        presTot += document.getElementById('imp-cuo-'+i).value;
+        presTot += +document.getElementById('imp-cuo-'+i).value;
     }
     console.log(total);
     console.log(presTot);
